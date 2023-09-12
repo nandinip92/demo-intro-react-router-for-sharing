dd559b98fd3d9e34b453c46881a4e8fbfa7a6247
nandinip92 <nandini.p8892@gmail.com>
2023-09-12 13:24:29 +0100

    refactor: removing unnecessary imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import "./App.css";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Router } from "./components/router/router";
-
-import Home from "./components/home/home";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   );
 }
 
